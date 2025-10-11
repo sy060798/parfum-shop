0c36d106aa2e5522966e64995625d6fab9425e3f
Gog12 <bejop0433@gmail.com>
2025-10-11 08:43:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-document.getElementById("btnBeli").addEventListener("click", function() {
-  window.open("https://wa.me/6281234567890?text=Halo,%20saya%20ingin%20pesan%20Herbal%20Mint", "_blank");
-});
+// Script ini opsional, bisa ditambah efek animasi atau tracking nanti
+console.log("Website parfum aktif ✅");
