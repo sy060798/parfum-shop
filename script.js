a76c231a7bcedef05d8310d1d50de60bf5f40c69
Gog12 <bejop0433@gmail.com>
2025-10-11 08:36:35 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("btnBeli").addEventListener("click", function() {
+  window.open("https://wa.me/6281234567890?text=Halo,%20saya%20ingin%20pesan%20Herbal%20Mint", "_blank");
+});
